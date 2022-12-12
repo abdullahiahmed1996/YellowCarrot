47a419245dd5e4a03bddc5783eb59b5ad18f225a
Abdullahi Ahmed <abdullahi.ahmed@edu.newton.se>
2022-12-12 10:45:02 +0100

    Initial commit

---
+++ YellowCarrot/Models/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowCarrot.Models
+{
+    internal class Ingredient
+    {
+        public int IngridientId { get; set; }
+        public string Name { get; set; } = null!;
+        public int? Quantity { get; set; }
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; } = null!;
+    }
+}
---
+++ YellowCarrot/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YellowCarrot.Models
+{
+    internal class Recipe
+    {
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+
+      
+    }
+}
